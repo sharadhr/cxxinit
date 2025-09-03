8031d83e6dc1aada9115506f741c02820f189d6b
Sharadh Rajaraman <r.sharadh@outlook.sg>
2025-09-03 18:29:35 +0100

    Rework triplets and toolchains
    - Remove release specific triplets
    - Rework MSVC toolchain for Asan, LTO, etc

--- tools/cmake/toolchains/cl-msvc.cmake
+++ tools/cmake/toolchains/cl-msvc.cmake
@@ -19,79 +19,73 @@ set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 # Some useful flags
 if (NOT DEFINED _VCPKG_ROOT_DIR)
-    string(JOIN " " WARNING_FLAGS
-        "/sdl"
-        "/guard:cf"
-        "/utf-8"
-        "/diagnostics:caret"
-        "/w14165"
-        "/w44242"
-        "/w44254"
-        "/w44263"
-        "/w34265"
-        "/w34287"
-        "/w44296"
-        "/w44365"
-        "/w44388"
-        "/w44464"
-        "/w14545"
-        "/w14546"
-        "/w14547"
-        "/w14549"
-        "/w14555"
-        "/w34619"
-        "/w34640"
-        "/w24826"
-        "/w14905"
-        "/w14906"
-        "/w14928"
-        "/w45038"
-        "/W4"
-        "/permissive-"
-        "/volatile:iso"
-        "/Zc:inline"
-        "/Zc:preprocessor"
-        "/Zc:enumTypes"
-        "/Zc:lambda"
-        "/Zc:__cplusplus"
-        "/Zc:externConstexpr"
-        "/Zc:throwingNew"
-        "/Zf"
-        "/EHsc"
-    )
+	string(JOIN " " WARNING_FLAGS
+		"/sdl"
+		"/guard:cf"
+		"/utf-8"
+		"/diagnostics:caret"
+		"/w14165"
+		"/w44242"
+		"/w44254"
+		"/w44263"
+		"/w34265"
+		"/w34287"
+		"/w44296"
+		"/w44365"
+		"/w44388"
+		"/w44464"
+		"/w14545"
+		"/w14546"
+		"/w14547"
+		"/w14549"
+		"/w14555"
+		"/w34619"
+		"/w34640"
+		"/w24826"
+		"/w14905"
+		"/w14906"
+		"/w14928"
+		"/w45038"
+		"/W4"
+		"/permissive-"
+		"/volatile:iso"
+		"/Zc:inline"
+		"/Zc:preprocessor"
+		"/Zc:enumTypes"
+		"/Zc:lambda"
+		"/Zc:__cplusplus"
+		"/Zc:externConstexpr"
+		"/Zc:throwingNew"
+		"/Zf"
+		"/EHsc"
+	)
 endif ()
 string(JOIN " " LINKER_FLAGS
-    "/DEBUG"
-    "/guard:cf"
+	"/LTCG:INCREMENTAL"
+	"/DEBUG"
+	"/OPT:REF"
+	"/OPT:NOICF"
+	"/guard:cf"
 )
 
 string(JOIN " " ASAN_FLAGS
-    "/O2"
-    "/fsanitize=address"
+	"/O2"
+	"/fsanitize=address"
+	"/D_DISABLE_VECTOR_ANNOTATION"
+	"/D_DISABLE_STRING_ANNOTATION"
 )
 string(JOIN " " RTC_FLAGS
-    "/Od"
-    "/RTC1"
+	"/Od"
+	"/RTC1"
 )
 
 set(FLAG_TYPES "C" "CXX")
-
-# Add a new build type: LTO, equal to Release but with LTO
-if ($ENV{BUILD_WITH_LTO})
+foreach (CONFIG "ASAN" "RTC")
 	foreach (FLAG_TYPE ${FLAG_TYPES})
-		set(CMAKE_${FLAG_TYPE}_FLAGS_LTO "/O2" CACHE STRING "" FORCE)
+		set(CMAKE_${FLAG_TYPE}_FLAGS_${CONFIG} "${${CONFIG}_FLAGS} /GL /Gw /Gy" CACHE STRING "" FORCE)
 	endforeach ()
-
-	set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
-	set(CMAKE_MAP_IMPORTED_CONFIG_LTO "Release" "RelWithDebInfo" "MinSizeRel")
-endif ()
-
-
-foreach(CONFIG "ASAN" "RTC")
-    foreach(FLAG_TYPE ${FLAG_TYPES})
-        set(CMAKE_${FLAG_TYPE}_FLAGS_${CONFIG} "${${CONFIG}_FLAGS}" CACHE STRING "" FORCE)
-    endforeach()
-endforeach()
+	set(CMAKE_MAP_IMPORTED_CONFIG_${CONFIG} "Release" "RelWithDebInfo" "MinSizeRel" "")
+endforeach ()
 
 # Set the default flags
 set(CMAKE_C_FLAGS_INIT ${WARNING_FLAGS})
--- tools/vcpkg-overlays/triplets/x64-windows-clang-static-md-release.cmake
+++
@@ -1,8 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
-
-set(VCPKG_BUILD_TYPE release)
-
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../../../tools/cmake/toolchains/clang-msvc.cmake")
-set(VCPKG_LOAD_VCVARS_ENV ON)
